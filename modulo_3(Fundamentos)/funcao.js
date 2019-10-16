038bea9494c8a9fc0c27e7d2270ef4d4204ca467
CaioFML <caio.felipemotta@hotmail.com>
2019-10-16 02:26:13 -0300

    27. Exemplos Básicos de Funções #01

---
+++ modulo_3(Fundamentos)/funcao.js
@@ -0,0 +1,17 @@
+// funcao sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 3, 6, 7, 21)
+imprimirSoma()
+
+// funcao com retorno
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
