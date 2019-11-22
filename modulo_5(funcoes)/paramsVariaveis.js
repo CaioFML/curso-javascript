52396bc6d2d769f15e9d78c80adbce7899a6cf18
CaioFML <caio.felipemotta@hotmail.com>
2019-11-22 10:09:44 -0300

    65. Parâmetros Variáveis

---
+++ modulo_5(funcoes)/paramsVariaveis.js
@@ -0,0 +1,16 @@
+// arguments é uma palavra reservada, é um array com todos os argumentos da função
+
+function soma() {
+	let soma = 0
+	for(i in arguments) {
+		soma += arguments[i]
+	}
+	return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+
+console.log(soma(1.1, 2.2, "Teste"))
+console.log(soma("a", "b", "c"))
