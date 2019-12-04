889f8e217209ec2f2790cb7ef8bb81ab4b0ca615
CaioFML <caio.felipemotta@hotmail.com>
2019-12-04 17:20:55 -0300

    108. Foreach #02

---
+++ modulo_7(Array)/forEach2.js
@@ -0,0 +1,11 @@
+const aprovados = ["Agatha", "Aldo", "Daniel", "Raquel"]
+
+Array.prototype.forEach2 = function(callback) {
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i], i, this)
+  }
+}
+
+aprovados.forEach2(function(nome, index) {
+  console.log(`${index + 1}) ${nome}`)
+})
