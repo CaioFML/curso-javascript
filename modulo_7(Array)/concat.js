b665d375a5ca96271d7500cf10ef8c2d04250869
CaioFML <caio.felipemotta@hotmail.com>
2019-12-06 15:24:55 -0300

    118. Concat

--- modulo_7(Array)/concat.js
+++ modulo_7(Array)/concat.js
@@ -4,4 +4,4 @@ const todos = filhas.concat(filhos, "Fulano")
 
 console.log(todos, filhas, filhos)
 
-console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+console.log(["a"].concat([1, 2], [3, 4], 5, [[6, 7]]))
