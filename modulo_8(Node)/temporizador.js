286598704aa8893f4800d8e8be86dbcd780eadee
CaioFML <caio.felipemotta@hotmail.com>
2020-02-17 12:14:11 -0300

    142. Tarefas Agendadas com Temporizador

---
+++ modulo_8(Node)/temporizador.js
@@ -0,0 +1,23 @@
+const schedule = require("node-schedule")
+
+const tarefa1 = schedule.scheduleJob("*/5 * * * * *", function () {
+  console.log("Executndo tarefa 1", new Date().getSeconds())
+})
+
+setTimeout(function () {
+  tarefa1.cancel()
+  console.log("Cancelndo tarefa 1")
+}, 20000)
+
+// setImmediate
+// setInterval
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 12
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+  console.log("Executando tarefa 2!", new Date(), getSeconds())
+})
+s
