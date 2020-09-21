94db8e086ac2c893f9d9c1b0cfb10560ccf33164
CaioFML <caio.felipemotta@hotmail.com>
2020-09-21 20:31:34 -0300

    165. Promise #05

---
+++ modulo_9(ESNext)/promise_5.js
@@ -0,0 +1,23 @@
+function funcionarOuNao(valor, chanceErro) {
+  return new Promise((resolve, reject) => {
+    try {
+      con.log("temp")
+      if(Math.random() < chanceErro) {
+        reject("Ocorreu um erro!")
+      } else {
+        resolve(valor)
+      }
+    } catch(e) {
+      reject(e)
+    }
+  })
+}
+
+funcionarOuNao("testando", 0.5)
+  .then(v => `Valor: ${v}`)
+  .then(
+    v => console.log(v),
+    err => console.log(`Erro Esp.: ${err}`)
+  )
+  .catch(err => console.log(`Erro geral: ${err}`))
+  .then(() => console.log("Fim!"))
